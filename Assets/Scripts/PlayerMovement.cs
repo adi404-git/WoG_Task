3ac4da6cf7dfabc52138b012cae4503149177647
adi404-git <ajhaji977.com>
2025-12-24 19:37:45 +0530

    Boundary and Sprinting added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,44 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float sprintMultiplier = 2f; 
     private Vector2 moveInput;
+    private bool isSprinting; 
     private Animator animator;
+
     void Start()
     {
-           animator= GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
+
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
 
+    public void OnSprint(InputValue value)
+    {
+        isSprinting = value.isPressed;
+    }
+
     void Update()
     {
-        transform.position += (Vector3)moveInput * moveSpeed * Time.deltaTime;
+       
+        float Speed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
 
-        bool IsMoving = moveInput.sqrMagnitude > 0.01;
-        animator.SetBool("IsMoving",IsMoving);
+        
+        transform.position += (Vector3)moveInput * Speed * Time.deltaTime;
 
-        if (IsMoving){
-            animator.SetFloat("x",moveInput.x);
-            animator.SetFloat("y",moveInput.y);
+        bool IsMoving = moveInput.sqrMagnitude > 0.01f;
+        animator.SetBool("IsMoving", IsMoving);
+
+        if (IsMoving)
+        {
+            animator.SetFloat("x", moveInput.x);
+            animator.SetFloat("y", moveInput.y);
         }
     }
-    
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
-    // Update is called once per frame
-   
 }
